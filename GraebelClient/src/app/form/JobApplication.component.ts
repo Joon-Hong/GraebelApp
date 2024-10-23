75c10864154acc0cd16f53499982ac8643077021
Joon-Hong <joon.hong1998@gmail.com>
2024-10-23 01:01:33 -0400

    Added Cover letter to form Updated country to be more dynamic

--- GraebelClient/src/app/form/JobApplication.component.ts
+++ GraebelClient/src/app/form/JobApplication.component.ts
@@ -10,7 +10,7 @@ export class JobApplicationComponent {
   countries = ['U.S.', 'Canada'];
   states = ['ON', 'QE', 'AB'];
 
-  application = new JobApplication('Joon Hong', 'i am super duper qualified', 'i have got this', this.states[0], this.countries[1]);
+  application = new JobApplication('Joon Hong', 'i am super duper qualified', 'I can do it!', this.states[0], this.countries[1]);
 
   submitted = false;
 
